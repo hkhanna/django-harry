9e21b6c443e5db21a4aadb5051b5de01a7cd2aab
Harry Khanna <harry@khanna.law>
2026-03-09 21:24:28 -0400

    Register email models in admin

--- src/harry/email/admin.py
+++ src/harry/email/admin.py
@@ -1,1 +1,24 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import EmailMessage, EmailMessageAttachment
+
+
+class EmailMessageAttachmentInline(admin.TabularInline):
+    model = EmailMessageAttachment
+    extra = 0
+
+
+@admin.register(EmailMessage)
+class EmailMessageAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "to_email", "status", "created_at"]
+    list_filter = ["status", "created_at"]
+    search_fields = ["to_email", "to_name", "subject", "message_id"]
+    readonly_fields = [
+        "uuid",
+        "created_at",
+        "updated_at",
+        "message_id",
+        "esp_event",
+        "esp_event_at",
+    ]
+    inlines = [EmailMessageAttachmentInline]
--- src/harry/email/apps.py
+++ src/harry/email/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class EmailConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "harry.email"
+    verbose_name = "Harry"
 
     def ready(self):
         import harry.email.signals  # noqa: F401
